866aa0864195661eccc8089fd88a3a31d0579a14
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-19 12:45:08 +0000

    Update week 3 exercise 2.py

--- week 3 exercise 2.py
+++ week 3 exercise 2.py
@@ -1,5 +1,6 @@
 """This file contains the code for the Week 3 lab exercises,
 focusing on for loops, while loops, and GitHub integration."""
 
-for i in range(1, 11):#print numbers 1 to 10
-    print(i)
+for i in range(1, 21):#print even numbers between 1 to 20
+    if (i % 2) == 0:
+        print(i)
