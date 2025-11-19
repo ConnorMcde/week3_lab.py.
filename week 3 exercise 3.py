9435d6a3c00bdfa5c40ffb5165f1aabe0ea1d947
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-19 13:16:44 +0000

    Update week 3 exercise 3.py

--- week 3 exercise 3.py
+++ week 3 exercise 3.py
@@ -1,6 +1,6 @@
 """Week 3 exercise 3 pattern"""
 
-for i in range(5): #prints a 5 x 5 square of asterix
-    for i in range(5):
+for i in range(1, 6): #prints a right angle triangle of asterix
+    for j in range(i):
         print("*",end='')
     print()
