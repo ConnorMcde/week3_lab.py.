4191dc106677a070783a7dce59e3325638345506
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-19 12:26:02 +0000

    Add files via upload

---
+++ week 3 exercise 2.py
@@ -0,0 +1,5 @@
+"""This file contains the code for the Week 3 lab exercises,
+focusing on for loops, while loops, and GitHub integration."""
+
+for i in range(1, 11):#print numbers 1 to 10
+    print(i)
